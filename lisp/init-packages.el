df031103957babbce6bc4aa54b15fbc2d471624f
jameswq <392331433@qq.com>
2018-04-05 10:43:51 +0800

    add init-packages.el

---
+++ lisp/init-packages.el
@@ -0,0 +1,30 @@
+(require 'package)
+(package-initialize)
+(setq package-archives '(
+                         ("gnu"   . "http://elpa.emacs-china.org/gnu/")
+                         ("melpa" . "http://elpa.emacs-china.org/melpa/")
+                         ("melpa-stable" . "http://mirrors.tuna.tsinghua.edu.cn/elpa/melpa-stable/")
+                         )
+      )
+
+
+;; find executable Path on OS X3
+(when (memq window-system '(mac ns))
+  (exec-path-from-shell-initialize))
+
+(require 'hungry-delete)
+(global-hungry-delete-mode t)
+
+                                        ;(ivy-mode 1)
+                                        ;(setq ivy-use-virtual-buffers t)
+                                        ;(setq enable-recursive-minibuffers t)
+
+;; open global Company complete
+(global-company-mode 1)
+
+(load-theme 'monokai)
+
+(require 'popwin)
+(popwin-mode t)
+
+(provide 'init-packages)
